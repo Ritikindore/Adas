2b6f849d1ec099977f3d55b590ae3f0143bda72a
Ritikindore <49471472+Ritikindore@users.noreply.github.com>
2019-05-22 20:10:37 +0530

    Update speed_sensor.c

--- speed_sensor.c
+++ speed_sensor.c
@@ -13,10 +13,20 @@ int n1=120;
                        PORTB|=(1<<PB4);
                        _delay_ms(10);
                 }
-               else 
-                {
-                       PORTB&=~(1<<PB4);
-                       PORTB|=(1<<PB3);
-                       _delay_ms(10);
-                }
-}
+               else if(sensor<=n)
+               {
+                       PORTB&=~(1<<PB3);
+                       PORTB|=(1<<PB4);
+                        _delay_ms(200);
+                       }//normal
+
+          else
+          {
+               PORTB&=~(1<<PB3);
+                PORTB&=~(1<<PB4);
+          }}
+           else
+          {
+               PORTB&=~(1<<PB3);
+                PORTB&=~(1<<PB4);
+                }}
